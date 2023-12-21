9b16481e83e2de992243fe119dc81413cb295fa6
mahinbarde98 <129951475+mahinbarde98@users.noreply.github.com>
2023-12-21 20:07:47 +0530

    Add files via upload
    Added files

---
+++ src/main/java/com/cts/favorite/FavouritteListApplication.java
@@ -0,0 +1,13 @@
+package com.cts.favorite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavouritteListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavouritteListApplication.class, args);
+	}
+
+}
